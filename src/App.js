4bbad81a5bed818133b02eed2ce97ec30728630f
Justin Ochoa <just.och92@gmail.com>
2025-06-08 11:03:48 -0400

    added product match and recommondation

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Axios from "axios";
 function App() {
   const [search, setSearch] = useState("");
   const [pairing, setPairing] = useState([]);
+  const [title, setTitle] = useState([]);
 
   const getRecs = async () => {
     let response = await Axios.get(
       `https://api.spoonacular.com/food/wine/pairing?apiKey=${process.env.REACT_APP_API_KEY}&food=${search}`
     );
-    console.log(response);
+    setPairing(response.data);
+    setTitle(search);
   };
   return (
     <div className="App">
@@ -19,11 +21,48 @@ function App() {
         <span>Enter you dinner below to receive our wine recommendation</span>
       </div>
       <div className="input-container">
-        <input type="text" onChange={(e) => setSearch(e.target.value)} />
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
         <button type="button" onClick={getRecs}>
           Search
         </button>
       </div>
+      {pairing.pairedWines?.length === 0 ? (
+        <div className="no-wine-container">
+          <span>Sorry no recommendations! :(</span>
+        </div>
+      ) : (
+        <div className="wine-list">
+          {pairing.pairedWines && <span>{`Wines that go with ${title}`}:</span>}
+          <p>{pairing.pairingText}</p>
+          <ul>
+            {pairing.pairedWines?.map((el, i) => {
+              return <li key={i}>{el}</li>;
+            })}
+            <div className="recs">
+              {pairing.pairedWines && <span>Product Match:</span>}
+              {pairing.productMatches?.map((el, i) => {
+                return (
+                  <div key={i}>
+                    <img src={el.imageUrl} alt={el.title} />
+                    <span>{el.title}</span>
+                    <p className="description">{el.description}</p>
+                    <p>Price: {el.price}</p>
+                    <p>
+                      <a href={el.link} rel="noreferrer" target="_blank">
+                        Where to buy{" "}
+                      </a>
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
